48927df7dc84baee182ffc99d76e442b558f9697
Ivalo <823583560@qq.com>
2018-09-05 15:31:24 +0800

    Merge branches 'devZK' and 'master' of github.com:MceOrg/Mce-shop into devZK
    # Conflicts:
    #	Mce-shop.iml

--- src/main/java/com/mce/shop/controller/CustLoginServlet.java
+++ src/main/java/com/mce/shop/controller/CustLoginServlet.java
@@ -1,4 +1,4 @@
-package com.mce.shop.servlet;
+package com.mce.shop.controller;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CustomerServlet extends HttpServlet {
+public class CustLoginServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
